ec84a863b14e9cf733cdafe341dffc5d79ae3fd1
RoopeshRameshwar <roopesh.rameshwar@students.mq.edu.au>
2024-09-05 19:57:51 +1000

    Initial commit

--- Wk7 tutorial script.R
+++ Wk7 tutorial script.R
@@ -5,4 +5,9 @@ usethis::use_git_config(user.name = "RoopeshRameshwar",
                         user.email = "roopesh.rameshwar@students.mq.edu.au")
 usethis::create_github_token()
 
+
+gitcreds::gitcreds_set()
+gh::gh_whoami()
+usethis::git_sitrep()
+usethis::use_git()
 # Wk6 topics in Wk7 tutorial class
